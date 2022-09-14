764bdf3a77f8996138b4c75dcf0ecb150b6e4039
Jordan <jordantylerharris@gmail.com>
2022-09-14 15:09:31 -0400

    we have weather data accessed

--- assets/js/results.js
+++ assets/js/results.js
@@ -22,13 +22,14 @@ if (element && element.bulmaCarousel) {
 //WEATHER CODE
 // Placeholder until user input is available
 let apiKey = "&appid=d34f0b45996f55d571b6eceb335266c3";
-let city = "Houston";
+let city = "Houston"; //document.querySelector("#city");
 let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial" + apiKey;
 let weatherBtn = document.querySelector("#weatherBtn");
 let modalWindow = document.querySelector(".modal");
 
 weatherBtn.addEventListener("click", () => {
 	    modalWindow.classList.add("is-active");
+		getWeather(queryUrl);
 });
 function getWeather(queryUrl) {
     fetch(queryUrl)
@@ -37,12 +38,17 @@ function getWeather(queryUrl) {
         })
         .then((data) => {
             console.log(data);
-            let zip = data[0].postal_code;
-            console.log(zip);
+            // let zip = data[0].postal_code;
+            // console.log(zip);
         });
 }
 
-getWeather(queryUrl);
+let closeBtn = document.querySelector("#close-button");
+closeBtn.addEventListener("click", () => {
+	modalWindow.classList.remove("is-active");
+});
+
+
 
 // TESTING
 // event listener
